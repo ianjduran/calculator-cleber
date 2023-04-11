5a66387b9c313817ba62f1972e0d394c606f3263
Ian Duran <ianjduran@gmail.com>
2023-04-11 17:10:38 -0600

    fix equal ui

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -14,12 +14,12 @@ const btnValues = [
 export default function Calculator(){
     return(
         <>
-            <div className='w-72 bg-slate-950 p-2 flex-col space-y-4 rounded-md'>
+            <div className='block w-72 bg-slate-950 p-2 flex-col space-y-4 rounded-md'>
                 <Screen value={'0'} className={'overflow-hidden block bg-slate-900 h-16 text-gray-50 text-end align-middle p-2 text-4xl'}/>
                 <ButtonBox>
                     {btnValues.flat().map((btn, i)=>{
                         return(
-                            <Button className={`text-xl hover:brightness-110 p-2 py-4 font-semibold rounded-md text-white ${btn === "=" ? "bg-amber-600" : "bg-slate-700"}`}
+                            <Button className={`text-xl hover:brightness-110 p-2 py-4 font-semibold rounded-md text-white ${btn === "=" ? "bg-amber-600 col-span-2" : "bg-slate-700"}`}
                              key={i} value={btn} onClick={() => console.log(`${btn} pressed`)}/>
                         )
                     })}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Calculator from '@/components/Calculator'
 
 const inter = Inter({ subsets: ['latin'] })
 
 
-
 export default function Home() {
   return (
-    <main className="min-h-screen flex-col items-center justify-between px-24 py-10">
+    <main className="min-h-screen flex-col items-center justify-center px-24 py-10">
       <h1 className='text-4xl mb-4 font-semibold'>Javascript Calculator</h1>
       <Calculator/>
     </main>
