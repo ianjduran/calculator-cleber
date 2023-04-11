6db6649f32aa2b50b81931913dcab0758a2cc1c0
Ian Duran <ianjduran@gmail.com>
2023-04-11 15:32:22 -0600

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const btnValues = {}
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+    </main>
+  )
+}
