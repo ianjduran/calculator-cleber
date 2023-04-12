ec7643ef887f85bd01aaa96e595e465ea5099bbc
Ian Duran <ianjduran@gmail.com>
2023-04-12 11:51:29 -0600

    add math eval and buttons logic

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -8,85 +8,84 @@ const btnValues = [
     [7, 8, 9, "/"],
     [4, 5, 6, "X"],
     [1, 2, 3, "-"],
-    ["C", 0, "+-", "+"],
+    ["AC", 0, "+-", "+"],
     ["="],
   ];
 
 
-const calculateResult = (a: number, b:number, sign:string) =>
-  sign === "+" ? a + b : 
-  sign === "-" ? a - b : 
-  sign === "X" ? a * b : 
-                 a / b;
 
-const calcnitialValues={
-    sign: "",
-    num: 0,
-    res: 0
+
+const calcInitial={
+    expression: "",
+    res: ""
 }
+
 
 export default function Calculator(){
 
-    const [calc, setCalc] = useState(calcnitialValues)
-
-    const handleReset = () => {
-        setCalc(calcnitialValues)
-    }
+    const [calc, setCalc] = useState(calcInitial)
 
 
-    const handleNumberClick = (e: any) =>{
+    const handleButtonClick = (e: any) =>{
         e.preventDefault()
-        const value = e.target.innerHTML;
-        // Ir almacenando con setState
+        let btnValue = e.target.innerHTML;
+        switch(btnValue){
+            case "AC":
+                setCalc(calcInitial)
+                break
+            case "+-":
+                setCalc({
+                    ...calc,
+                    res: calc.res? (parseInt(calc.res) * -1).toString() : calc.res
+                })
+                break
+            case "=":
+                try{
+                    const result = eval(calc.expression)
+                    setCalc({
+                        ...calc,
+                        res: result
+                    })
+                }
+                catch(err: any){
+                    setCalc({
+                        ...calcInitial,
+                        res: String(err.name)
+                    })
+                }
+                break
+            default:
+                if(btnValue=="X")
+                    btnValue="*"
+                if(calc.res!==""){
+                    setCalc({
+                        ...calc,
+                        expression: calc.res
+                    })
+                }
+                setCalc({
+                    ...calcInitial,
+                    expression: calc.expression + btnValue
+                    
+                })
+                break
+
+        }
         
     }
 
-
-    const handleSignBtnClick = (e:any)=>{
-        e.preventDefault()
-        const val = e.target.innerHTML; 
-        // Almacenar el valor del signo
-        // algo como res = calculateResult(res,num,sign)
-        try {
-            // Calcular los valores y almacenarlo en calc.res con el setState
-
-        }
-        catch(err){
-            // Handle division por 0 o NaN
-        }
-       
-
-    }
-
-    // this 99% sure wrong
-    const handleInvertBtnClick = (e:any)=>{
-        e.preventDefault()
-        const val = e.target.innerHTML;
-
-        setCalc({
-            ...calc,
-            sign: val,
-            res: !calc.res ? calc.res*(-1) : 0,
-            num: !calc.num ? calc.num*(-1) : 0,
-        })
-
-    }
-
-    
+  
 
     return(
         <>
             <div className='block w-72 bg-slate-950 p-2 flex-col space-y-4 rounded-lg'>
-                <Screen value={calc.num? calc.num : calc.res} className={'overflow-hidden block bg-slate-900 h-16 text-gray-50 text-end align-middle p-2 text-4xl rounded-md'}/>
+                <Screen value={calc.res? calc.res : calc.expression} className={'overflow-hidden block bg-slate-900 h-16 text-gray-50 text-end align-middle p-2 text-4xl rounded-md'}/>
                 <ButtonBox>
                     {btnValues.flat().map((btn, i)=>{
                         return(
                             <Button className={`text-xl hover:brightness-110 p-2 py-4 font-semibold rounded-md text-white ${btn === "=" ? "bg-amber-600 col-span-4" : "bg-slate-700"}`}
                              key={i} value={btn} onClick={
-                                btn==="C" ? handleReset :
-                                btn==="+-" ? handleInvertBtnClick :
-                                btn==="+" || btn==="-" || btn==="X" || btn==="/" || btn==="="? handleSignBtnClick :
-                                handleNumberClick
+                                handleButtonClick
 
                              }/>
                         )
