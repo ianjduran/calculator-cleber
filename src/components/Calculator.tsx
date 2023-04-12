47b7172207b245df1d8ab6ddd4ee651b733dd725
Ian Duran <ianjduran@gmail.com>
2023-04-12 10:05:50 -0600

    create hook logic

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,26 +1,94 @@
 import Screen from './Screen'
 import ButtonBox from './ButtonBox'
 import Button from './Button';
+import { useState } from 'react';
 
 
 const btnValues = [
-    ["C", "+-", "%", "/"],
-    [7, 8, 9, "X"],
-    [4, 5, 6, "-"],
-    [1, 2, 3, "+"],
-    [0, ".", "="],
+    [7, 8, 9, "/"],
+    [4, 5, 6, "X"],
+    [1, 2, 3, "-"],
+    ["C", 0, "+-", "+"],
+    ["="],
   ];
 
+
+const calculateResult = (a: number, b:number, sign:string) =>
+  sign === "+" ? a + b : 
+  sign === "-" ? a - b : 
+  sign === "X" ? a * b : 
+                 a / b;
+
+const calcnitialValues={
+    sign: "",
+    num: 0,
+    res: 0
+}
+
 export default function Calculator(){
+
+    const [calc, setCalc] = useState(calcnitialValues)
+
+    const handleReset = () => {
+        setCalc(calcnitialValues)
+    }
+
+
+    const handleNumberClick = (e: any) =>{
+        e.preventDefault()
+        const value = e.target.innerHTML;
+        // Ir almacenando con setState
+        
+    }
+
+
+    const handleSignBtnClick = (e:any)=>{
+        e.preventDefault()
+        const val = e.target.innerHTML; 
+        // Almacenar el valor del signo
+        // algo como res = calculateResult(res,num,sign)
+        try {
+            // Calcular los valores y almacenarlo en calc.res con el setState
+
+        }
+        catch(err){
+            // Handle division por 0 o NaN
+        }
+       
+
+    }
+
+    // this 99% sure wrong
+    const handleInvertBtnClick = (e:any)=>{
+        e.preventDefault()
+        const val = e.target.innerHTML;
+
+        setCalc({
+            ...calc,
+            sign: val,
+            res: !calc.res ? calc.res*(-1) : 0,
+            num: !calc.num ? calc.num*(-1) : 0,
+        })
+
+    }
+
+    
+
     return(
         <>
-            <div className='block w-72 bg-slate-950 p-2 flex-col space-y-4 rounded-md'>
-                <Screen value={'0'} className={'overflow-hidden block bg-slate-900 h-16 text-gray-50 text-end align-middle p-2 text-4xl'}/>
+            <div className='block w-72 bg-slate-950 p-2 flex-col space-y-4 rounded-lg'>
+                <Screen value={calc.num? calc.num : calc.res} className={'overflow-hidden block bg-slate-900 h-16 text-gray-50 text-end align-middle p-2 text-4xl rounded-md'}/>
                 <ButtonBox>
                     {btnValues.flat().map((btn, i)=>{
                         return(
-                            <Button className={`text-xl hover:brightness-110 p-2 py-4 font-semibold rounded-md text-white ${btn === "=" ? "bg-amber-600 col-span-2" : "bg-slate-700"}`}
-                             key={i} value={btn} onClick={() => console.log(`${btn} pressed`)}/>
+                            <Button className={`text-xl hover:brightness-110 p-2 py-4 font-semibold rounded-md text-white ${btn === "=" ? "bg-amber-600 col-span-4" : "bg-slate-700"}`}
+                             key={i} value={btn} onClick={
+                                btn==="C" ? handleReset :
+                                btn==="+-" ? handleInvertBtnClick :
+                                btn==="+" || btn==="-" || btn==="X" || btn==="/" || btn==="="? handleSignBtnClick :
+                                handleNumberClick
+
+                             }/>
                         )
                     })}
                 </ButtonBox>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="min-h-screen flex-col items-center justify-center px-24 py-10">
-      <h1 className='text-4xl mb-4 font-semibold'>Javascript Calculator</h1>
+      <h1 className='text-4xl font-bold'>Cleber - Calculator</h1>
+      <h2 className='font-bold mb-4 text-gray-700'>Ian Javier Duran Roman</h2>
       <Calculator/>
     </main>
   )
